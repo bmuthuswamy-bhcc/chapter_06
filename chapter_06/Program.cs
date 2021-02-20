a55dd324c8cddb8f7f71769fb94b9edccc6116e7
bharathwaj <b.muthuswamy@bhcc.edu>
2021-02-19 20:24:16 -0500

    Add project files.

---
+++ chapter_06/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * Bunker Hill Coommunity College Spring 2021
+ * Chapter 6 - Generics
+ */
+
+using System;
+
+namespace chapter_06 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
