ace21a3d824c563b1c81c22267250aa2d73b4f72
bharathwaj <b.muthuswamy@bhcc.edu>
2021-02-19 20:37:15 -0500

    Added devel branch and simple Generic Class

--- chapter_06/Program.cs
+++ chapter_06/Program.cs
@@ -6,9 +6,21 @@
 using System;
 
 namespace chapter_06 {
+    class GenericDemo<T> { // T will hold the type
+        public T value { get; private set; }
+
+        public GenericDemo(T value) {
+            this.value = value;
+        }
+
+        public override string ToString() => $"{typeof(T)} : {this.value}";
+    }
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+        var obj1 = new GenericDemo<int>(33);
+        var obj2 = new GenericDemo<string>("Hello, world!");
+        Console.WriteLine(obj1);
+        Console.WriteLine(obj2);
         }
     }
 }
