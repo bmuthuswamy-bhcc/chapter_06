c920d17ff5887c3a95874823d72e2bec6b25e9fa
bharathwaj <b.muthuswamy@bhcc.edu>
2021-02-20 11:19:43 -0500

    Added Contravariant types and Type  parameter constraints
    This patch shows:
    1. Contravariant type example by adding a shape comparison interface
    2.  "Numeric" type parameter constraints to GenericDemo

--- chapter_06/Program.cs
+++ chapter_06/Program.cs
@@ -4,9 +4,21 @@
  */
 
 using System;
+using System.Collections.Generic;
 
 namespace chapter_06 {
-    public class GenericDemo<T> { // T will hold the type
+    /* Illustration of type parameter constraints
+     * We are restricting T to be either integer or double (numeric)
+     * Unfortunately, there is no explicit constraint to define a numeric type
+     * but the constraint below represent the best combination.
+     * For details, refer to p. 194 in your textbook
+     */
+    public class GenericDemo<T>
+            where T : struct, 
+                      IComparable, IComparable<T>, 
+                      IConvertible,
+                      IEquatable<T>,
+                      IFormattable {
         public T value { get; private set; }
 
         public GenericDemo(T value) {
@@ -16,22 +28,22 @@ namespace chapter_06 {
         public override string ToString() => $"{typeof(T)} : {this.value}";
     }
 
-    // Now illustrate inheritance and generic interfaces
-    public interface IShape<T> {
-        public T Area { get;  }
+    // Now illustrate contravariant concept in Generics
+    public interface IShape {
+        public double Area { get;  }
     }
 
-    public class Square : IShape<int> { // need to explicitly define type here
-        public int length { get; set; }
+    public class Square : IShape { // need to explicitly define type here
+        public double length { get; set; }
 
-       public Square (int length) {
+       public Square (double length) {
             this.length = length;
         }
 
-        public int Area => this.length * this.length;
+        public double Area => this.length * this.length;
     }
 
-    public class Circle : IShape<double> {
+    public class Circle : IShape {
         public double radius { get; set; }
 
         public Circle(double radius) {
@@ -41,19 +53,75 @@ namespace chapter_06 {
         public double Area => Math.PI * this.radius * this.radius;
     }
 
+    /*
+     * Shape comparer will compare the areas of two shapes s1 and s2, will return:
+     * -1 if area(s1) < area(s2)
+     * 0 if area(s1) == area(s2)
+     * > 0 if area(s1) > area(s2)
+     */
+    public class ShapeComparer : IComparer<IShape> { 
+        public int Compare(IShape s1, IShape s2) {
+            if (s1 is null) {
+                return s2 is null ? 0 : -1;
+            }
+            if (s2 is null) {
+                return 1;
+            }
+            return s1.Area.CompareTo(s2.Area);
+        }
+    }
+
+    // Square: compare the length of sides
+    public class SquareComparer : IComparer<Square> {
+        public int Compare(Square s1, Square s2) {
+            if (s1 is null) {
+                return s2 is null ? 0 : -1;
+            }
+            if (s2 is null) {
+                return 1;
+            }
+            return s1.length.CompareTo(s2.length);
+        }
+    }
+
+    // Circle : compare length of radii
+    public class CircleComparer : IComparer<Circle> {
+        public int Compare(Circle c1, Circle c2) {
+            if (c1 is null) {
+                return c2 is null ? 0 : -1;
+            }
+            if (c2 is null) {
+                return 1;
+            }
+            return c1.radius.CompareTo(c2.radius);
+        }
+    }
     class Program {
         static void Main(string[] args) {
             var obj1 = new GenericDemo<int>(33);
-            var obj2 = new GenericDemo<string>("Hello, world!");
+            /* Declaration below will give a compile-time error with the type
+             * parameter restriction to numerics
+             */
+            // var obj2 = new GenericDemo<string>("Hello, world!");
             Console.WriteLine(obj1);
-            Console.WriteLine(obj2);
+            //Console.WriteLine(obj2);
 
             Square objSquare = new Square(10);
             Circle objCircle = new Circle(1);
 
+            ShapeComparer objShapeComparer = new ShapeComparer();
+
             Console.WriteLine($"The area of the square is {objSquare.Area} unit^2");
             Console.WriteLine($"The area of circle is {objCircle.Area} unit^2");
 
+            if (objShapeComparer.Compare(objSquare,objCircle) > 0 ) {
+                Console.WriteLine($"{objSquare} has larger area than {objCircle}");
+            } else if (objShapeComparer.Compare(objSquare, objCircle) == 0) {
+                Console.WriteLine($"{objSquare} and {objCircle} have equal area");
+            } else {
+                Console.WriteLine($"{objSquare} has smaller area than {objCircle}");
+            }
+            
         }
     }
 }
